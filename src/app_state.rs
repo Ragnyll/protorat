cc1ca75331f66a01554471793e057da297a0f5eb
ragnyll <ragnyll@gallowzhumour.dev>
2025-02-02 19:59:24 -0800

    allow unused directions and use ci

--- src/app_state.rs
+++ src/app_state.rs
@@ -65,7 +65,9 @@ pub enum AppStateUpdate {
 /// A relative direciton
 #[derive(PartialEq, Eq, Debug, Clone)]
 pub enum Direction {
+    #[allow(unused)]
     Up,
+    #[allow(unused)]
     Down,
     Left,
     Right,
